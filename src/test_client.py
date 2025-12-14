4bf8bf3b9d50760e3776f0805d56bc954facbdf3
EmiRoberti77 <emi@datamotion.ai>
2025-12-14 13:05:43 +0000

    test_client and server

---
+++ src/test_client.py
@@ -0,0 +1,47 @@
+import asyncio
+import aiohttp
+import json
+
+BUFFER = 65536
+
+async def message(index:int = 1):
+    async with aiohttp.ClientSession() as session:
+        endpoint = f"https://jsonplaceholder.typicode.com/todos/{index}"
+        async with session.get(endpoint) as resp:
+            json_data = await resp.json()
+            return json_data
+
+
+async def start(reader:asyncio.StreamReader, writer:asyncio.StreamWriter):
+    index = 1
+    while True:
+        json_message = await message(index=index)
+        encoded_message = json.dumps(json_message).encode('utf-8')
+        writer.write(encoded_message)
+        await writer.drain()
+        index += 1
+        data = await reader.read(BUFFER)
+        if not data:
+            writer.close()
+            await writer.wait_closed()
+            break
+        
+        print(data.decode())
+        await asyncio.sleep(2)
+
+    
+
+async def main():
+    reader, writer = await asyncio.open_connection('127.0.0.1', 8888)
+    if reader and reader != None:
+        task = asyncio.create_task(start(reader, writer))
+        if task:
+            print('task created')
+            await asyncio.gather(task)
+
+
+if __name__ == "__main__":
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        pass
---
+++ src/test_server.py
@@ -0,0 +1,43 @@
+import asyncio
+
+BUFFER = 65536
+
+async def close_writer(writer:asyncio.StreamWriter):
+    try:
+        if not writer.is_closing():
+            print(f'closing writer')
+            writer.close()
+            await writer.wait_closed
+    except Exception:
+        pass
+
+async def handle_client(reader:asyncio.StreamReader, writer:asyncio.StreamWriter):
+    addr = writer.get_extra_info('peername')
+    print(f'connected into handle client {addr}')
+    while True:    
+        try:
+            data = await reader.read(BUFFER)
+            if not data:
+                break
+            
+            writer.write(data)
+            await writer.drain()
+        except Exception as e:
+            print(e)
+    
+    await close_writer(writer)
+        
+
+async def main():
+    server = await asyncio.start_server(handle_client, '0.0.0.0', 8888)
+    addr = ', '.join(str(sock.getsockname()) for sock in server.sockets)
+    print(f'Serving on {addr}')
+    
+    async with server:
+        await server.serve_forever()
+
+if __name__ == "__main__":
+    asyncio.run(main=main())
+
+
+
